27f93924377335bf9d47b7c770e08b26e663d10c
galileor97 <galwicakso@gmail.com>
2024-08-07 17:23:24 +0700

    Image delete function is done

--- Server/controllers/ImageController.js
+++ Server/controllers/ImageController.js
@@ -1,10 +1,26 @@
+const { Image } = require('../models/index')
+
+
 class ImageController {
 
-    static async createImage(req, res) {
+    static async deleteImage(req, res, next) {
         try {
-            
+            let { id } = req.params
+            let image = await Image.searchByPk(id)
+
+            if (!image) {
+                throw { name: 'Not-Found' }
+            }
+            await Image.destroy({
+                where: {
+                    id: id
+                }
+            })
+
+            res.status(200).json({ message: `Image with id ${id} successfully deleted` })
         } catch (error) {
             console.log(error);
+            next(error)
         }
     }
 }
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
-const UserController = require('../controllers/UserController')
-const errorHandler = require('../middleware/errorHandler')
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+
 const PredictionController = require('../controllers/PredictionController')
+const UserController = require('../controllers/UserController')
 const isAuthenticate = require('../middleware/isAuthenticate')
+const errorHandler = require('../middleware/errorHandler')
 
 router.post('/login', UserController.login)
 router.post('/register', UserController.register)
 
 
-router.post('/predict', isAuthenticate, PredictionController.createPrediction)
+router.post('/predict', isAuthenticate, upload.fields([
+    { name: 'base_image', maxCount: 1 },
+    { name: 'style_image', maxCount: 1 },
+    { name: 'identity_image', maxCount: 1 },
+    { name: 'composition_image', maxCount: 1 },
+]), PredictionController.createPrediction)
 // router.post()
+
+router.post('/predict2', PredictionController.prediction2)
 router.use(errorHandler)
 
 module.exports = router;
