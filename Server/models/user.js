2ad2c469556c68287e61bf382886d1ab2f2d38b4
galileor97 <galwicakso@gmail.com>
2024-08-07 10:32:44 +0700

    Server project setup is done

---
+++ Server/models/user.js
@@ -0,0 +1,73 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // User.hasMany()
+       User.hasMany(models.Image,{foreignKey:'userId'})
+    }
+  }
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Email is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Email is required'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Password is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Password is required'
+        }
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Username is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Username is required'
+        }
+      }
+    },
+    credit: {
+      type:DataTypes.INTEGER,
+      min:{
+        args:0,
+        msg:'Unificient credit'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
